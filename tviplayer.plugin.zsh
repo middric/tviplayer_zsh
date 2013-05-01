27c07c4d681ab8dd9d2d470e675c0a108f082fa0
Rich Middleditch <richmiddleditch@gmail.com>
2013-05-01 11:29:56 +0100

    adding aliases

--- tviplayer.plugin.zsh
+++ tviplayer.plugin.zsh
@@ -1,3 +1,10 @@
+alias v1='cd ~/workspace/tviplayer'
+alias v3='cd ~/workspace/iplayer'
+alias sandbox='ssh root@192.168.192.10'
+alias svs='svn st | grep "style/stylesheets" -v'
+alias svd='svs | cut -b 3- | xargs svn diff'
+alias svr='open "/Applications/Google Chrome.app" $(svd | revue)'
+
 function trunk {
     branch trunk
 }
@@ -5,16 +12,16 @@ function trunk {
 function branch {
     if [[ -d .svn ]]; then
         if [ $1 ]; then
-	    root=$(pwd | sed -n 's:^.*/::p')
-            url=$(svn info | sed -n s/URL:\ //p 2>/dev/null)	    	
+            root=$(pwd | sed -n 's:^.*/::p')
+            url=$(svn info | sed -n s/URL:\ //p 2>/dev/null)
             dir="$1"
-	    if [ $1 != "trunk" ]; then
-		dir="branches/$1"
-	    fi
-	    branch=$(echo $url | sed -n 's:'"$root"'/.*$:'"$root"'/'"$dir"':p')
-	    if [ $branch ]; then
-		svn switch $branch
-	    fi
+            if [ $1 != "trunk" ]; then
+                dir="branches/$1"
+            fi
+            branch=$(echo $url | sed -n 's:'"$root"'/.*$:'"$root"'/'"$dir"':p')
+            if [ $branch ]; then
+                svn switch $branch
+            fi
         else
             echo "No branch defined"
         fi
